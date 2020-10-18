b340f4337a34a1406ec8446c979a67a5a7362420
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-19 02:41:31 +0530

    Program to demonstrate TERNARY Operator in c

---
+++ TERNARY.c
@@ -0,0 +1,20 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate TERNARY Operator in c.
+Author  : Chpaone09®
+Date    : Oct 19, 2020
+
+************************************************************
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+    
+    int a = 2, b = 5;
+    
+    printf("\n\n\t Bigger Number is >> %d", a > b ? a : b);
+    
+}
